09f42cee1ef7b9feef9dd4e1fd6ea34d6b9dacbe
taisaxton <taisaxton@gmail.com>
2026-08-21 09:20:23 +1200

    Fixed File Updates, Replaced Old Files

---
+++ FDWT.py
@@ -0,0 +1,15 @@
+import random, sys
+import pygame # pip install pygame
+import pygame_widgets # pip install pygame_widgets
+from pygame_widgets.button import Button
+
+
+# Pygame Setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Five Days With Tony") 
+clock = pygame.time.Clock()
+
+    
+pygame.quit()
+sys.exit()
