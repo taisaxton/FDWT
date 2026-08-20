2f5e24cd0c76bfbf998f4c2fab40c4ef076486b7
taisaxton <taisaxton@gmail.com>
2026-08-21 09:22:46 +1200

    Update Window, Test mouse positions

--- FDWT.py
+++ FDWT.py
@@ -10,6 +10,28 @@ screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Five Days With Tony") 
 clock = pygame.time.Clock()
 
+
+run = True
+while run:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            run = False
+            quit()
+
+    mouse = pygame.mouse.get_pos()                                    
+    
+    
+    print (mouse)
+    
+    # Screen Reset
+    screen.fill("black")
+
+    clock.tick(60)  # Frames Per Second
+    
+    pygame_widgets.update(events)
+    pygame.display.update()
     
 pygame.quit()
 sys.exit()
